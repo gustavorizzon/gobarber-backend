ce44304b0c52d9cd96d674672bdb1cff2e3454c6
gustavorizzon <gustavop.rizzon@gmail.com>
2021-07-31 10:42:56 -0300

    feat: tests

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,42 @@
+import { uuid } from 'uuidv4';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '../../infra/typeorm/entities/User';
+
+class FakeUsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const foundUser = this.users.find(user => user.email === email);
+
+    return foundUser;
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const foundUser = this.users.find(user => user.id === id);
+
+    return foundUser;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const foundIndex = this.users.findIndex(u => u.id === user.id);
+
+    this.users[foundIndex] = user;
+
+    return user;
+  }
+}
+
+export default FakeUsersRepository;
