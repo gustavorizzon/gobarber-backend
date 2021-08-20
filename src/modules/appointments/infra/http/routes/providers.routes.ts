e50b7e1da005c94b5e56237c48f039a9dbeb1a45
gustavorizzon <gustavop.rizzon@gmail.com>
2021-08-19 21:48:39 -0300

    feat(validation): celebrate request validation

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
@@ -16,12 +17,33 @@ const providerDayAvailabilityController =
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
 providersRouter.get(
   '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      month: Joi.number().integer().min(1).max(12).required(),
+      year: Joi.number().integer().positive().required(),
+    },
+  }),
   providerMonthAvailabilityController.index,
 );
+
 providersRouter.get(
   '/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      day: Joi.number().integer().min(1).max(31).required(),
+      month: Joi.number().integer().min(1).max(12).required(),
+      year: Joi.number().integer().positive().required(),
+    },
+  }),
   providerDayAvailabilityController.index,
 );
 
