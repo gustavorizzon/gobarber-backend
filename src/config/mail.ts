dc04bd8dbfa1951681c2a4a36ab207582120a8d3
gustavorizzon <gustavop.rizzon@gmail.com>
2021-08-24 21:01:23 -0300

    feat(AWS-SES): SES Mail Provider

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'name@example.com',
+      name: 'Name from Example.com',
+    },
+  },
+} as IMailConfig;
